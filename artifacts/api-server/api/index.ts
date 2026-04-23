cff7f6b38b88aac3da1eb7c27db6fa28d6128e24
KASAKBATRA <arorakasak2005@gmail.com>
2026-04-23 22:57:12 +0530

    cannot get

--- artifacts/api-server/api/index.ts
+++ artifacts/api-server/api/index.ts
@@ -1,3 +1,13 @@
-import app from "../src/app.js";
+import express from "express";
+import cors from "cors";
+import router from "../src/routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
 
 export default app;
